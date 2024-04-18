1ee1fa26a79b69202389775c7239ec18b7eafe45
gui0r <guimichel41@gmail.com>
2024-04-18 15:56:25 -0300

    atividade 12

--- exercicio12ST.cpp
+++ exercicio12ST.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
-#include <string>
-
-using namespace std;
-
-
-int contarVogais(char* str, int index = 0, int count = 0) {
-    if (str[index] == '\0')
-        return count;
-    
-    if (str[index] == 'a' || str[index] == 'e' || str[index] == 'i' || str[index] == 'o' || str[index] == 'u' ||
-        str[index] == 'A' || str[index] == 'E' || str[index] == 'I' || str[index] == 'O' || str[index] == 'U') {
-        count++;
-    }
-
-    return contarVogais(str, index + 1, count);
-}
+#include <locale.h>
+#include <cstring>
 
 int main() {
     char frase[20];
     cout << "Digite uma frase: ";
     getline(frase, cin);
+
+    if (*str == '\0') {
+        return 0;
+    }
+
+    if (strchr("aeiouAEIOU", *str) != nullptr) {
+        return 1 + contarVogais(str + 1);
+    }
+
+    return contarVogais(str + 1);
 
     int numVogais = contarVogais(frase);
 
